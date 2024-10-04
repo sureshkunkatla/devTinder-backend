d95280806b9d90eb75b37446f8f0e6fefd78cc20
suresh kunkatla <suresh.kunkatla7@gmail.com>
2024-10-04 22:44:24 +0530

    modified useRouter

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const userRouter = express.Router();
 
@@ -54,6 +55,37 @@ userRouter.get("/connections", userAuth, async (req, res) => {
     res.status(200).json({ data });
   } catch (e) {
     res.status(400).send({ message: e.message });
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedinUser = req.user;
+    let page = parseInt(req.query.page);
+    let limit = parseInt(req.query.limit);
+    limit = limit > 30 ? 30 : limit;
+    const skip = (page - 1) * limit;
+
+    const connectionrequests = await ConnectionRequest.find({
+      $or: [{ toUserId: loggedinUser._id }, { fromUserId: loggedinUser._id }],
+    }).select(["fromUserId", "toUserId"]);
+
+    const hideAlreadyConnections = new Set();
+    connectionrequests.forEach((each) => {
+      hideAlreadyConnections.add(each.toUserId);
+      hideAlreadyConnections.add(each.fromUserId);
+    });
+
+    const remainingusers = await User.find({
+      _id: { $nin: Array.from(hideAlreadyConnections) },
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.status(200).json({ data: remainingusers });
+  } catch (e) {
+    res.status(400).json({ message: e.message });
   }
 });
 
